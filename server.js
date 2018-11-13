74537fedccfd8996cf1c44bb09b8a230e334d120
Adelino <adelino.reis@outlook.com>
2018-11-13 17:06:18 -0200

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.listen(3000, function() {
+    console.log('server running on port 3000')
+})
+app.get('/',(req, res) => {
+    res.render('index.ejs')
+})
+app.set('view engine', 'ejs')
+app.post('/show', (req, res) => {
+    console.log(req.body)
+})
